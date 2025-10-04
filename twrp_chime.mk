fb02c1a322c2256521958841249f67aab02b0dfb
AnGgIt886 <jarbull86@gmail.com>
2025-10-04 20:13:05 +0700

    chime: call aosp base

--- twrp_chime.mk
+++ twrp_chime.mk
@@ -10,6 +10,7 @@ DEVICE_PATH := device/xiaomi/chime
 PRODUCT_RELEASE_NAME := chime
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
